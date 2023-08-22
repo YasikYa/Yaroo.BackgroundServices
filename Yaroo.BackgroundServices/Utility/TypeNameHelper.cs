88202aa3c5da770459d77c6758be3a38ccf82f62
yaros.vodotyka@gmail.com <yaros.vodotyka@gmail.com>
2023-08-22 20:26:45 +0300

    improve GetTypeName method

--- Yaroo.BackgroundServices/Utility/TypeNameHelper.cs
+++ Yaroo.BackgroundServices/Utility/TypeNameHelper.cs
@@ -9,6 +9,14 @@
 
         public static string GetTypeName(Type type)
         {
+            if (type.IsGenericType)
+            {
+                string genericArguments = type.GetGenericArguments()
+                                    .Select(x => GetTypeName(x))
+                                    .Aggregate((x1, x2) => $"{x1}, {x2}");
+                return $"{type.Name.Substring(0, type.Name.IndexOf("`"))}"
+                     + $"<{genericArguments}>";
+            }
             return type.Name;
         }
     }
